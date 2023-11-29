d685aecb15a99d4f3cc366f7bd395db8380a366e
Maksym Lapshyn <maksym.lapshyn@gmail.com>
2023-11-29 17:51:51 +0200

    Fix glyphicons.

---
+++ wwwroot/js/carousel.js
@@ -0,0 +1,18 @@
+$('.carousel[data-type="multi"] .item').each(function () {
+    let next = $(this).next();
+
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (let i = 0; i < 2; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
